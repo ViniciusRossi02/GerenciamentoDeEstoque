c4c2e47c2548245c20e0fa208fd2c4949748c05b
unknown <rossivini1010@gmail.com>
2025-12-21 21:55:31 -0300

    Começando o beack-end do usuari/pegando informações do form,e codificando senha

--- usuarios/inserir.php
+++ usuarios/inserir.php
@@ -1,6 +1,28 @@
  <?php
 
     require_once __DIR__ ."/../config.php";
+    require_once BASE_PATH . "/src/utils.php";
+
+     $erro = null;
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+       $nome = $_POST['nome'];
+       $email = $_POST['email'];
+       $senhaForm = $_POST['senha'];
+
+       if(empty($nome) || empty($email) || empty($senhaForm)){
+          $erro = "Preencha todos os campos!!";
+       }else {
+          // CODIFICAR A SENHA
+          $senhaCodificada = codificarSenha($senhaForm);
+          echo $senhaCodificada;
+          // ENVIAR OS DADOS PARA O BANCO
+          // inserirUsuario();
+          // REDIRECIONAR PARA A LISTA DE USUARIO
+       }
+    }
+
+
      $titulo = "Adicionar Usuário |";
     require_once BASE_PATH ."/includes/cabecalho.php";
 ?>
@@ -8,20 +30,24 @@
 <section class=" mb-4 border rounded-3 p-4 border-primary-subtle">
      <h3 class="text-center"><i class="bi bi-plus-circle-fill"></i> Adicionar Usuário</h3>
      
+    <?php if($erro):?>
+     <p class="alert alert-danger text-center"><?= $erro ?></p>
+     <?php endif; ?>
+
      <form action="" method="POST" class="w-75 mx-auto">
           <div class="form-group">
                <label for="nome" class="form-label">Nome:</label>
-               <input type="text" name="nome" id="nome" class="form-control">
+               <input  type="text" name="nome" id="nome" class="form-control">
           </div>
 
            <div class="form-group">
                <label for="email" class="form-label">E-mail:</label>
-               <input type="email" name="email" id="email" class="form-control">
+               <input  type="email" name="email" id="email" class="form-control">
           </div>
 
           <div class="form-group">
                <label for="senha" class="form-label">Senha:</label>
-               <input type="password" name="senha" id="senha" class="form-control">
+               <input  type="password" name="senha" id="senha" class="form-control">
           </div>
 
           <button class="btn btn-success my-4" type="submit">
